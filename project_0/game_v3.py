2cda66c3a7d72b81d1118f510ec158cfb8d61aab
Zylno <tbdk02@gmail.com>
2022-08-16 18:33:18 +0300

    edit game_v2. py and add game_v3.py

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -19,28 +19,5 @@ def random_predict(number:int=1) -> int:
             break # выход из цикла, если угадали
     return(count)
 
-def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
 
-    Args:
-        random_predict ([type]): функция угадывания
-
-    Returns:
-        int: среднее количество попыток
-    """
-
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
-
-    for number in random_array:
-        count_ls.append(random_predict(number))
-
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
-
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
-
-# RUN
 print(f'Количество попыток: {random_predict(15)}')
-score_game(random_predict)
