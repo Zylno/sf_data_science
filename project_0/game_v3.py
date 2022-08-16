be6253501c79d5fdb8a83fceeff0f141ffed06fb
Zylno <tbdk02@gmail.com>
2022-08-16 19:20:55 +0300

    edit game_v3.py

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -11,13 +11,20 @@ def random_predict(number:int=1) -> int:
     """
 
     count = 0
+    min = 1
+    max = 101
+    predict_number = np.random.randint(min, max) # предполагаемое число
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
-        if number == predict_number:
+        mid = round((min+max)/2) 
+        if number > predict_number:
+            max = mid
+        elif number < predict_number:
+            min = mid 
+        else:
             break # выход из цикла, если угадали
     return(count)
 
-
 print(f'Количество попыток: {random_predict(15)}')
+
